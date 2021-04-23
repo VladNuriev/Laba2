fcc1220ab39fc61b97348359a07cd61a12145166
VladNuriev <vlad.nuriev.12@mail.ru>
2021-04-23 09:28:17 +0300

    first commit

---
+++ script/2.bat
@@ -0,0 +1,14 @@
+echo off 
+rem go to into firstcat
+cd firstcat
+rem create firstfile
+echo > firstfile.txt
+rem go to into secondfile
+cd secondcat
+rem create secondlife
+echo > secondfile.txt
+rem go to into thirdfile
+cd thirdcat
+rem create thirdfile
+echo > thirdfile.txt
+pause
